87a3902c0eb37f52c0368bf97d52b5afc76663d3
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-08-14 08:44:16 +0200

    Got the debugger working

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,16 @@
 
 int main()
 {
-    std::cout << "Hello, World! The setup works!" << std::endl;
+    int x = 0;
+    std::cout << "Hello, World! The setup works" << x << "\n";
+    x = 1;
+    std::cout << "Hello, World! The setup works" << x << "\n";
+    x = 2;
+    std::cout << "Hello, World! The setup works" << x << "\n";
+    x = 3;
+    std::cout << "Hello, World! The setup works" << x << "\n";
+    x = 4;
+    std::cout << "Hello, World! The setup works" << x << "\n";
+
     return 0;
 }
